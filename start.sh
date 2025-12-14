866babfcc34837987da026233f77e949a7ee419e
KJMAN678 <kj.fireman5@gmail.com>
2025-12-14 13:33:06 +0900

    レビュー対応.

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 uv run manage.py migrate
+uv run manage.py collectstatic --noinput
 uv run manage.py createsuperuser --noinput || true
 uv run python manage.py sample_command 3
 # uv run manage.py runserver 0.0.0.0:8000
