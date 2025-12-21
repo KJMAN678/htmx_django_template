83f237f1b28f22463e97e2256cd9232e3c3b66ef
koji <kj.fireman5@gmail.com>
2025-12-21 18:33:12 +0900

    Enable static file serving in urls.py
    Add static file serving to urlpatterns.

--- web/config/urls.py
+++ web/config/urls.py
@@ -1,13 +1,16 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("sample.urls")),
     path("accounts/", include("allauth.urls")),
-]
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += debug_toolbar_urls()
