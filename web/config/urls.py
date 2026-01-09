390b5c494ba6a1ec4a7456416962aca6c491fd63
KJMAN678 <kj.fireman5@gmail.com>
2026-01-09 20:17:02 +0900

    linter formatter 実行.

--- web/config/urls.py
+++ web/config/urls.py
@@ -4,12 +4,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("sample.urls")),
     path("accounts/", include("allauth.urls")),
-] 
+]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
